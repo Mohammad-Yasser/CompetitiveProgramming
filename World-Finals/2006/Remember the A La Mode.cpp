7a2ad8700b59cdab7bfab61de288511b7784b041
Mohammad Yasser <mohammadyasser3@gmail.com>
2016-02-16 17:01:27 +0200

    Update Remember the A La Mode.cpp

--- World-Finals/2006/Remember the A La Mode.cpp
+++ World-Finals/2006/Remember the A La Mode.cpp
@@ -1,8 +1,4 @@
-#include <bits/stdc++.h> 
-using namespace std;
-typedef long long ll;
-#define getP(x) (x + 2)
-#define getC(x) (x + 2 + p)
+
 const int N = 103, M = N * (N - 1), INF = 1e3;
 const int SRC = 0, SNK = 1;
 struct Edge {
@@ -30,7 +26,7 @@ void addBi(int u, int v, int cap, int cost) {
 bool inQ[N] ;
 int dist[N], flow[N], parent[N];
 
-pair<int, int> bellman() {
+pair<int, int> spfa() {
 	queue<int> q;
 	memset(dist, 0x3f, sizeof dist);
 	memset(flow, 0, sizeof flow);
@@ -69,54 +65,8 @@ pair<int, int> bellman() {
 pair<int, int> mcmf() {
 	int cst = 0, flow = 0;
 	pair<int, int> cf;
-	while (cf = bellman(), cf.second)
+	while (cf = spfa(), cf.second)
 		cst += cf.first * cf.second, flow += cf.second;
 	return {cst,flow};
 }
 
-void reverse() {
-	for (int i = 0; i < edgeCnt; i += 2) {
-		edges[i].cap += edges[i | 1].cap, edges[i | 1].cap = 0;
-		edges[i].cost *= -1, edges[i | 1].cost *= -1;
-	}
-}
-
-int main() {
-	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-#ifndef ONLINE_JUDGE
-	freopen("in.txt", "r", stdin);
-#endif
-	cout << fixed << setprecision(2) ;
-	int p, c;
-	int cs = 0 ;
-	while (cin >> p >> c, p) {
-		init();
-
-		for (int i = 0; i < p; ++i) {
-			int tmp;
-			cin >> tmp;
-			addBi(SRC, getP(i), tmp, 0);
-		}
-		for (int i = 0; i < c; ++i) {
-			int tmp;
-			cin >> tmp;
-			addBi(getC(i), SNK, tmp, 0);
-		}
-
-		for (int i = 0; i < p; ++i)
-			for (int j = 0; j < c; ++j) {
-				double tmp;
-				cin >> tmp;
-				if (tmp != -1)
-				  addBi(getP(i), getC(j), INF, round(tmp * 100));
-			}
-
-
-
-		int mn = mcmf().first;
-		reverse();
-		int mx = -mcmf().first;
-		cout << "Problem " << ++cs << ": " << mn / 100.0 << " to " << mx / 100.0 << '\n';
-	}
-
-}

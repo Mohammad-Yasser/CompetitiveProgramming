5d3f342f0a4457e5066177a69e7a7c203069926a
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-07-15 22:32:39 +0200

    Add ccw sweep

---
+++ Library/Geometry/anticlockwise_sweep.cpp
@@ -0,0 +1,31 @@
+// Assumes no 3 collinear points.
+Long sweep(vector<P> points) {
+  sort(all(points), antiClockwiseComparator);
+  for (int i = 0; i < sz(points); ++i) {
+    int j = (i + 1) % sz(points);
+    if (points[i].cross(points[j]) < 0) {
+      rotate(begin(points), begin(points) + j, end(points));
+      break;
+      // To prevent having two consecutive points having angle more than 180,
+      // or in other words, p[i].cross(p[i+1]) < 0.
+    }
+  }
+  int l = 0, r = 1 % sz(points);
+
+  while (points[l].cross(points[r]) > 0) {
+    r = (r + 1) % sz(points);
+  }
+  for (auto& p : points) {
+    if (points[l].cross(p) < 0) {
+    }
+  }
+
+  while (l + 1 < sz(points)) {
+    ++l;
+    assert(points[l - 1].cross(points[l]) > 0);
+    while (points[l].cross(points[r]) > 0) {
+      r = (r + 1) % sz(points);
+    }
+  }
+  return res;
+}

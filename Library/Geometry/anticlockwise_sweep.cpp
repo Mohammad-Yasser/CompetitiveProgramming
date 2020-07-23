bd231ea7bd33635a3ac9e6e02ee4ac45c4d0fdbf
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-07-23 09:04:46 +0200

    Polishing

--- Library/Geometry/anticlockwise_sweep.cpp
+++ Library/Geometry/anticlockwise_sweep.cpp
@@ -1,5 +1,5 @@
 // Assumes no 3 collinear points.
-Long sweep(vector<P> points) {
+void sweep(vector<P> points) {
   sort(all(points), antiClockwiseComparator);
   for (int i = 0; i < sz(points); ++i) {
     int j = (i + 1) % sz(points);
@@ -13,19 +13,22 @@ Long sweep(vector<P> points) {
   int l = 0, r = 1 % sz(points);
 
   while (points[l].cross(points[r]) > 0) {
+    // update
     r = (r + 1) % sz(points);
   }
   for (auto& p : points) {
     if (points[l].cross(p) < 0) {
+      // update
     }
   }
 
   while (l + 1 < sz(points)) {
     ++l;
+    // update
     assert(points[l - 1].cross(points[l]) > 0);
     while (points[l].cross(points[r]) > 0) {
       r = (r + 1) % sz(points);
+      // update
     }
   }
-  return res;
 }
--- Library/Maths/LDE_with_pos_coefficients.cpp
+++ Library/Maths/LDE_with_pos_coefficients.cpp
@@ -1,6 +1,6 @@
 // Hmm , needs good understanding.
 
-ll egcd(ll a, ll b, ll &x, ll &y) {
+ll egcd(ll a, ll b, ll& x, ll& y) {
   if (a == 0) {
     x = 0;
     y = 1;
@@ -12,24 +12,20 @@ ll egcd(ll a, ll b, ll &x, ll &y) {
   y = x1;
   return d;
 }
-ll lcm(ll a, ll b) {
-  return a * b / __gcd(a, b);
-}
+ll lcm(ll a, ll b) { return a * b / __gcd(a, b); }
 
-
-bool solve(ll x , ll y , ll c, ll& a , ll& b) {
-	  // x and y are coprime.
-      egcd(x, y, a, b); // a * x + b * y == 1
-      a *= c;
-      b *= c;
-      if (b < 0) {
-        swap(a, b);
-        swap(x, y);
-      }
-      ll bmul = (lcm(x, y) / y);
-      ll amul = (lcm(x, y) / x);
-      ll mul = abs(a / amul) + 1;
-      a += mul * amul;
-      b -= mul * bmul;
+bool solve(ll x, ll y, ll c, ll& a, ll& b) {
+  // x and y are coprime.
+  egcd(x, y, a, b);  // a * x + b * y == 1
+  a *= c;
+  b *= c;
+  if (b < 0) {
+    swap(a, b);
+    swap(x, y);
+  }
+  ll bmul = (lcm(x, y) / y);
+  ll amul = (lcm(x, y) / x);
+  ll mul = abs(a / amul) + 1;
+  a += mul * amul;
+  b -= mul * bmul;
 }
-          

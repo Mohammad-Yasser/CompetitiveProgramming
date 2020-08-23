cfe854694a08e68f6e1e53f5e0a7a9bc07bd7ac0
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-08-23 16:53:31 +0200

    Enhance my CentroidDecomposition

--- Library/Trees/CentroidDecomposition.cpp
+++ Library/Trees/CentroidDecomposition.cpp
@@ -1,5 +1,33 @@
 // My modification of Anany's
 
+template <typename T, class F = function<T(const T&, const T&)>>
+struct SparseTable {
+  int n;
+  vector<vector<T>> sp;
+  F func;
+
+  SparseTable() {}
+
+  void init(const vector<T>& a, const F& f) {
+    func = f;
+    n = static_cast<int>(a.size());
+    int max_log = 32 - __builtin_clz(n);
+    sp.resize(max_log);
+    sp[0] = a;
+    for (int j = 1; j < max_log; ++j) {
+      sp[j].resize(n - (1 << j) + 1);
+      for (int i = 0; i <= n - (1 << j); ++i) {
+        sp[j][i] = func(sp[j - 1][i], sp[j - 1][i + (1 << (j - 1))]);
+      }
+    }
+  }
+
+  T query(int l, int r) const {
+    int lg = 32 - __builtin_clz(r - l + 1) - 1;
+    return func(sp[lg][l], sp[lg][r - (1 << lg) + 1]);
+  }
+};
+
 struct Edge {
   int to, weight;
   Edge(int to = 0, int weight = 0) : to(to), weight(weight) {}
@@ -8,43 +36,57 @@ struct Edge {
 struct Tree {
   vector<vector<Edge>> adj;
   int max_log;
-  vector<vector<int>> sparse_table;
   vector<Long> dist;
   vector<int> depth;
-  Tree(int n) : adj(n + 1), dist(n + 1), depth(n + 1) {
-    max_log = 32 - __builtin_clz(n + 1);
-    sparse_table.assign(max_log, vector<int>(n + 1));
+  vector<int> subtree_size;
+  vector<int> time_in;
+  vector<int> euler_tour;
+  vector<int> enter_index;   // First index of the node in the euler tour.
+  vector<int> time_to_node;  // time_to_node[i] is u s.t. time_in[u] == i.
+  SparseTable<int> sparse_table;
+
+  Tree(int n)
+      : adj(n + 1),
+        dist(n + 1),
+        depth(n + 1),
+        subtree_size(n + 1),
+        time_in(n + 1),
+        time_to_node(n + 1),
+        enter_index(n + 1) {}
+  void build() {
+    pDFS(1);
+    sparse_table.init(euler_tour, [&](int u, int v) {
+      return (time_in[u] < time_in[v]) ? u : v;
+    });
   }
   void addEdge(int u, int v, int w = 1) {
     adj[u].emplace_back(v, w);
     adj[v].emplace_back(u, w);
   }
   int size() const { return sz(adj); }
-  void pDFS(int u, int p) {
-    for (int k = 1; k < max_log; ++k)
-      sparse_table[k][u] = sparse_table[k - 1][sparse_table[k - 1][u]];
+  void pDFS(int u, int p = -1, int timer = 0) {
+    time_in[u] = timer++;
+    time_to_node[time_in[u]] = u;
+    subtree_size[u] = 1;
+    enter_index[u] = sz(euler_tour);
+    euler_tour.emplace_back(u);
+
     for (auto& edge : adj[u]) {
       int v = edge.to;
       if (v != p) {
         dist[v] = dist[u] + edge.weight;
         depth[v] = depth[u] + 1;
-        sparse_table[0][v] = u;
-        pDFS(v, u);
+        pDFS(v, u, timer);
+        euler_tour.emplace_back(u);
+        subtree_size[u] += subtree_size[v];
       }
     }
   }
   int getLCA(int u, int v) {
-    if (depth[u] > depth[v]) swap(u, v);
-    int k = depth[v] - depth[u];
-    for (int i = 0; i < max_log; i++)
-      if ((k >> i) & 1) v = sparse_table[i][v];
-    if (u == v) return u;
-    for (int i = max_log - 1; i >= 0; --i)
-      if (sparse_table[i][v] != sparse_table[i][u]) {
-        u = sparse_table[i][u];
-        v = sparse_table[i][v];
-      }
-    return sparse_table[0][u];
+    int l = enter_index[u];
+    int r = enter_index[v];
+    if (l > r) swap(l, r);
+    return sparse_table.query(l, r);
   }
   Long getDist(int u, int v) {
     return dist[u] + dist[v] - 2 * dist[getLCA(u, v)];
@@ -60,7 +102,7 @@ struct CentroidDecomposition {
     n = tree.size();
     sz = par = lvl = vector<int>(n);
     vis = vector<bool>(n);
-    this->tree.pDFS(1, -1);
+    this->tree.build();
     ConstructTree(1, -1);
   }
 
@@ -101,17 +143,19 @@ struct CentroidDecomposition {
     int x = u;
     while (~x) {
       // Update ancestor
+      line_container[x].addLine(penalty[u], tree.getDist(u, x));
       x = par[x];
     }
   }
-  int query(int u) {
+  Long query(int u) {
     int x = u;
-    int ret = 0;
+    Long res = LLONG_MAX;
     while (~x) {
       // Query ancestor
+      res = min(res, line_container[x].query(penalty[u]) + tree.getDist(u, x));
       x = par[x];
     }
-    return ret;
+    return res;
   }
 };
 

0f940f558189cf2cb16ca10d37c533ed14e7aa59
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-09-04 16:39:01 +0200

    Add another LiChaoTree

--- Library/Data Structures/LiChao_seg_tree.cpp
+++ Library/Data Structures/LiChao_seg_tree.cpp
@@ -1,207 +1,79 @@
-#include <bits/stdc++.h>
-#define endl '\n'
+/** Source:
+https://github.com/hieplpvip/cp-resource/blob/master/Data%20Structure/dynamic_lichaotree.cpp
+    Dynamic LiChao Segment Tree
+    Add lines/segments in arbitrary slope order. Query maximum/minimum value at
+x.
 
-using namespace std;
+    Time complexity: O(lgn) for each operation
+    Performance: roughly as fast as dynamic convex hull
+    Advantage: support segment, which convex hull can't
 
-const int64_t inf = (int64_t)1e17 + 42;
+    Tested on:
+    - https://codeforces.com/gym/101175/problem/F
+    - https://vn.spoj.com/problems/VOMARIO/ (can only be solved with LiChao
+Segment Tree since the objects are segments, not lines)
+    - https://csacademy.com/contest/archive/task/squared-ends
 
-struct LiChao_max
-{
-	struct line
-	{
-		int a, b;
-		line() { a = 0; b = 0; }
-		line(int _a, int _b) { a = _a; b = _b; }
-		int64_t eval(int x) { return a * 1ll * x + (int64_t)b; }
-	};
+**/
 
-	struct node
-	{
-		node *l, *r; line f;
-
-		node() { f = line(); l = nullptr; r = nullptr; }
-		node(int a, int b) { f = line(a, b); l = nullptr; r = nullptr; }
-		node(line v) { f = v; l = nullptr; r = nullptr; }
-	};
-
-	typedef node* pnode;
-
-	pnode root; int sz;
-	void init(int _sz) { sz = _sz + 1; root = nullptr; }
-
-	void add_line(int a, int b) { line v = line(a, b); insert(v, -sz, sz, root); }
-	int64_t query(int x) { return query(x, -sz, sz, root); }
-
-	void insert(line &v, int l, int r, pnode &nd)
-	{
-		if(!nd) { nd = new node(v); return; }
-
-		int64_t trl = nd->f.eval(l), trr = nd->f.eval(r);
-		int64_t vl = v.eval(l), vr = v.eval(r);
-
-		if(trl >= vl && trr >= vr) return;
-		if(trl < vl && trr < vr) { nd->f = v; return; }
-
-		int mid = (l + r) >> 1;
-		if(trl < vl) swap(nd->f, v);
-		if(nd->f.eval(mid) > v.eval(mid)) insert(v, mid + 1, r, nd->r);
-		else swap(nd->f, v), insert(v, l, mid, nd->l);
-	}
-
-	int64_t query(int x, int l, int r, pnode &nd)
-	{
-		if(!nd) return -inf;
-		if(l == r) return nd->f.eval(x);
-
-		int mid = (l + r) >> 1;
-		if(mid >= x) return max(nd->f.eval(x), query(x, l, mid, nd->l));
-		return max(nd->f.eval(x), query(x, mid + 1, r, nd->r));
-	}
-};
-
-//Mine:
-
-const int64_t inf = (int64_t) 1e17 + 42;
-
-struct LiChaoMax {
-  struct Line {
-    Long a, b;
-    Line() {
-      a = 0;
-      b = 0;
-    }
-    Line(Long _a, Long _b) {
-      a = _a;
-      b = _b;
-    }
-    Long eval(Long x) {
-      return a * 1ll * x + (Long) b;
-    }
+template <typename T, T minX, T maxX, T defVal, typename Function,
+          bool maximum = true>
+struct DynamicLiChaoTree {
+ private:
+  struct Function {
+    T a, b;
+    inline T calc(T x) const { return a * x + b; }
   };
-
   struct Node {
-    Node *l, *r;
-    Line f;
-
-    Node() {
-      f = Line();
-      l = nullptr;
-      r = nullptr;
-    }
-    Node(Long a, Long b) {
-      f = Line(a, b);
-      l = nullptr;
-      r = nullptr;
-    }
-    Node(Line v) {
-      f = v;
-      l = nullptr;
-      r = nullptr;
-    }
+    Line line = {0, maximum ? defVal : -defVal};
+    Node *lt = nullptr, *rt = nullptr;
   };
-
-  typedef Node* NodePointer;
-
-  NodePointer root;
-  int sz;
-  void init(int _sz) {
-    sz = _sz + 1;
-    root = nullptr;
-  }
-
-  void addLine(Long a, Long b) {
-    Line v = Line(a, b);
-    insert(v, -sz, sz, root);
-  }
-  Long query(int x) {
-    return query(x, -sz, sz, root);
-  }
-
-  void insert(Line &v, int l, int r, NodePointer &nd) {
-    if (!nd) {
-      nd = new Node(v);
-      return;
+  Node* root;
+  void update(Node* cur, T l, T r, T u, T v, Line nw) {
+    if (v < l || r < u) return;
+    T mid = (l + r) >> 1;
+    if (u <= l && r <= v) {
+      if (cur->line.calc(l) >= nw.calc(l)) swap(cur->line, nw);
+      if (cur->line.calc(r) <= nw.calc(r)) {
+        cur->line = nw;
+        return;
+      }
+      if (nw.calc(mid) >= cur->line.calc(mid)) {
+        if (!cur->rt) cur->rt = new Node();
+        update(cur->rt, mid + 1, r, u, v, cur->line);
+        cur->line = nw;
+      } else {
+        if (!cur->lt) cur->lt = new Node();
+        update(cur->lt, l, mid, u, v, nw);
+      }
+    } else {
+      if (!cur->rt) cur->rt = new Node();
+      if (!cur->lt) cur->lt = new Node();
+      update(cur->lt, l, mid, u, v, nw);
+      update(cur->rt, mid + 1, r, u, v, nw);
     }
-
-    int64_t trl = nd->f.eval(l), trr = nd->f.eval(r);
-    int64_t vl = v.eval(l), vr = v.eval(r);
-
-    if (trl >= vl && trr >= vr) return;
-    if (trl < vl && trr < vr) {
-      nd->f = v;
-      return;
-    }
-
-    int mid = (l + r) >> 1;
-    if (trl < vl) swap(nd->f, v);
-    if (nd->f.eval(mid) > v.eval(mid))
-      insert(v, mid + 1, r, nd->r);
-    else
-      swap(nd->f, v), insert(v, l, mid, nd->l);
   }
 
-  Long query(int x, int l, int r, NodePointer &nd) {
-    if (!nd) return -inf;
-    if (l == r) return nd->f.eval(x);
-
-    int mid = (l + r) >> 1;
-    if (mid >= x) return max(nd->f.eval(x), query(x, l, mid, nd->l));
-    return max(nd->f.eval(x), query(x, mid + 1, r, nd->r));
+ public:
+  // change l,r to add segment
+  void add(T a, T b, T l = minX, T r = maxX) {
+    if (!maximum) a = -a, b = -b;
+    update(root, minX, maxX, l, r, {a, b});
   }
-} tree;
-
-// Min
-
-struct LiChao_min
-{
-    struct line
-    {
-        int a, b;
-        line() { a = 0; b = 0; }
-        line(int _a, int _b) { a = _a; b = _b; }
-        int64_t eval(int x) { return a * 1ll * x + (int64_t)b; }
-    };
- 
-    struct node
-    {
-        node *l, *r; line f;
- 
-        node() { f = line(); l = nullptr; r = nullptr; }
-        node(int a, int b) { f = line(a, b); l = nullptr; r = nullptr; }
-        node(line v) { f = v; l = nullptr; r = nullptr; }
-    };
- 
-    typedef node* pnode;
- 
-    pnode root; int sz;
-    void init(int _sz) { sz = _sz + 1; root = nullptr; }
- 
-    void add_line(int a, int b) { line v = line(a, b); insert(v, -sz, sz, root); }
-    int64_t query(int x) { return query(x, -sz, sz, root); }
- 
-    void insert(line &v, int l, int r, pnode &nd)
-    {
-        if(!nd) { nd = new node(v); return; }
- 
-        int64_t trl = nd->f.eval(l), trr = nd->f.eval(r);
-        int64_t vl = v.eval(l), vr = v.eval(r);
- 
-        if(trl <= vl && trr <= vr) return;
-        if(trl > vl && trr > vr) { nd->f = v; return; }
- 
-        int mid = (l + r) >> 1;
-        if(trl > vl) swap(nd->f, v);
-        if(nd->f.eval(mid) < v.eval(mid)) insert(v, mid + 1, r, nd->r);
-        else swap(nd->f, v), insert(v, l, mid, nd->l);
+  T query(T x) {
+    Node* cur = root;
+    T res = cur->line.calc(x);
+    T l = minX, r = maxX, mid;
+    while (cur) {
+      res = max(res, cur->line.calc(x));
+      mid = (l + r) >> 1;
+      if (x <= mid)
+        cur = cur->lt, r = mid;
+      else
+        cur = cur->rt, l = mid + 1;
     }
- 
-    int64_t query(int x, int l, int r, pnode &nd)
-    {
-        if(!nd) return inf;
-        if(l == r) return nd->f.eval(x);
- 
-        int mid = (l + r) >> 1;
-        if(mid >= x) return min(nd->f.eval(x), query(x, l, mid, nd->l));
-        return min(nd->f.eval(x), query(x, mid + 1, r, nd->r));
-    }
+    if (!maximum) res = -res;
+    return res;
+  }
+  void init() { root = new Node; }
 };

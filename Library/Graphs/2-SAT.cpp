430b1387baf8ba7b806a6410569fd1c2f78d9df8
Mohammad Yasser <mohammadyasser3@gmail.com>
2020-09-04 16:40:51 +0200

    Modify 2-SAT

--- Library/Graphs/2-SAT.cpp
+++ Library/Graphs/2-SAT.cpp
@@ -17,21 +17,23 @@ vector<vector<int>> make_transpose_graph(const vector<vector<int>>& g) {
 }
 
 /**
- * @brief strongly connected components decomposition, Kosaraju's algorithm / 強連結成分分解
- * @return the pair (the number k of components, the function from vertices of g to components)
+ * @brief strongly connected components decomposition, Kosaraju's algorithm
+ * @return the pair (the number k of components, the function from vertices of g
+ * to components). The SCCs are sorted topologically.
  * @param g is an adjacent list of a digraph
  * @param g_rev is the transpose graph of g
  * @note $O(V + E)$
  */
 pair<int, vector<int>> decompose_to_strongly_connected_components(
-  const vector<vector<int>>& g, const vector<vector<int>>& g_rev) {
+    const vector<vector<int>>& g, const vector<vector<int>>& g_rev) {
   int n = g.size();
   vector<int> acc(n);
   {
     vector<bool> used(n);
     function<void(int)> dfs = [&](int i) {
       used[i] = true;
-      for (int j : g[i]) if (not used[j]) dfs(j);
+      for (int j : g[i])
+        if (not used[j]) dfs(j);
       acc.push_back(i);
     };
     for (int i = 0; i < n; ++i)
@@ -45,7 +47,8 @@ pair<int, vector<int>> decompose_to_strongly_connected_components(
     function<void(int)> rdfs = [&](int i) {
       used[i] = true;
       component_of[i] = size;
-      for (int j : g_rev[i]) if (not used[j]) rdfs(j);
+      for (int j : g_rev[i])
+        if (not used[j]) rdfs(j);
     };
     for (int i : acc)
       if (not used[i]) {
@@ -57,38 +60,43 @@ pair<int, vector<int>> decompose_to_strongly_connected_components(
 }
 
 pair<int, vector<int>> decompose_to_strongly_connected_components(
-  const vector<vector<int>>& g) {
+    const vector<vector<int>>& g) {
   return decompose_to_strongly_connected_components(g, make_transpose_graph(g));
 }
 
-auto node_ind =
-  [](int x, int n) {assert (x != 0 and abs(x) <= n); return ((abs(x)-1) << 1) | (x > 0);};
-auto opposite = [](int x) {return x ^ 1;};
+auto node_ind = [](int x, int n) {
+  assert(x != 0 and abs(x) <= n);
+  return ((abs(x) - 1) << 1) | (x > 0);
+};
+auto opposite = [](int x) { return x ^ 1; };
 
 vector<bool> twosat(int n, const vector<pair<int, int>>& cnf,
-  bool convert_to_node_ids = false) {
+                    bool convert_to_node_ids = false) {
   // make digraph
   vector<vector<int>> g(2 * n);
 
   for (auto it : cnf) {
     int x, y;
-    tie(x, y) = it; // x or y
+    tie(x, y) = it;  // x or y
     if (convert_to_node_ids) {
-      g[node_ind(-x, n)].push_back(node_ind(y, n)); // not x implies y
-      g[node_ind(-y, n)].push_back(node_ind(x, n)); // not y implies x
+      g[node_ind(-x, n)].push_back(node_ind(y, n));  // not x implies y
+      g[node_ind(-y, n)].push_back(node_ind(x, n));  // not y implies x
     } else {
-      g[opposite(x)].emplace_back(y); // not x implies y
-      g[opposite(y)].emplace_back(x); // not y implies x
+      g[opposite(x)].emplace_back(y);  // not x implies y
+      g[opposite(y)].emplace_back(x);  // not y implies x
     }
   }
   // do SCC
   vector<int> component = decompose_to_strongly_connected_components(g).second;
   vector<bool> valuation(n);
   for (int x = 1; x <= n; ++x) {
-    if (component[node_ind(x, n)] == component[node_ind(-x, n)]) { // x iff not x
-      return vector<bool>(); // unsat
+    if (component[node_ind(x, n)] ==
+        component[node_ind(-x, n)]) {  // x iff not x
+      return vector<bool>();           // unsat
     }
-    valuation[x - 1] = component[node_ind(x, n)] > component[node_ind(-x, n)]; // use components which indices are large
+    valuation[x - 1] =
+        component[node_ind(x, n)] >
+        component[node_ind(-x, n)];  // use components which indices are large
   }
   return valuation;
 }
